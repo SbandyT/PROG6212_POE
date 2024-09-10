9c1574015acf9d3706ef43bebabc9a3bedddd15d
Khano Tshivhandekano <st10298613@vcconnect.edu.za>
2024-09-10 20:58:31 +0200

    Added LecturerController

---
+++ Controllers/LecturerController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ST10298613_PROG6212_POE.Data;
+
+namespace ST10298613_PROG6212_POE.Controllers
+{
+    public class LecturerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LecturerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // View the list of claims
+        public IActionResult Index()
+        {
+            var lecturers = _context.Lecturers.ToList();
+            return View(lecturers);
+        }
+
+        // Submit a claim
+        [HttpGet]
+        public IActionResult SubmitClaim()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitClaim(Claim claim)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Claims.Add(claim);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(claim);
+        }
+    }
+}
