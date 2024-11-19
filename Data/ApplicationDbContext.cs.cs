e473360eaabbbc6cc35b5a9bbdffbdaae03ac3dc
Khano Tshivhandekano <st10298613@vcconnect.edu.za>
2024-11-19 14:00:12 +0200

    Part 3 Functionalties

--- Data/ApplicationDbContext.cs.cs
+++ Data/ApplicationDbContext.cs.cs
@@ -1,18 +1,19 @@
 ﻿using ST10298613_PROG6212_POE.Models;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace ST10298613_PROG6212_POE.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
         }
 
-        public DbSet<Lecturer> Lecturers { get; set; }
         public DbSet<Claim> Claims { get; set; }
-        public DbSet<SupportingDocument> SupportingDocuments { get; set; }
+        public DbSet<Lecturer> Lecturers { get; set; }
         public DbSet<Approval> Approvals { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using ST10298613_PROG6212_POE.Data;
 using Microsoft.EntityFrameworkCore;
-using ST10298613_PROG6212_POE.NewFolder;
+
+using Microsoft.AspNetCore.Identity;
 
 namespace ST10298613_PROG6212_POE
 {
@@ -11,12 +12,20 @@ namespace ST10298613_PROG6212_POE
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-            builder.Services.AddSignalR();
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+            })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
@@ -24,7 +33,6 @@ namespace ST10298613_PROG6212_POE
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -33,21 +41,15 @@ namespace ST10298613_PROG6212_POE
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Dashboard}/{id?}");
-            app.MapControllerRoute(
-            name: "lecturer",
-            pattern: "Lecturer/{action=Dashboard}/{id?}",
-            defaults: new { controller = "Lecturer" });
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
 
-            app.MapControllerRoute(
-                name: "admin",
-                pattern: "Admin/{action=Dashboard}/{id?}",
-                defaults: new { controller = "Admin" });
-            app.MapHub<ClaimStatusHub>("/claimStatusHub");
+            SeedRolesAndAdminUser(app.Services); // Add this method for seeding roles and an admin user.
 
             app.Run();
         }
